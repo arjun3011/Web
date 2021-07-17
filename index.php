4d5c2737afcc7d9b12bac8c8f32711f703bd20cd
Arjun Malik <67333225+arjun3011@users.noreply.github.com>
2021-07-17 11:44:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<h1>123</h1>
+<html>
+   <head>
+      <title>Home</title>
+   </head>
+   <body>
+        <?php include('archive-blog.php') ?>
+        <?php include('footer.php') ?>
+        <?php include('header.php') ?>
+      <?php include('front-page.php') ?>
+      <?php include('content-boxblog.php') ?>
+      <?php include('content-boxEvent.php') ?>
+      <?php include('content-child.php') ?>
+      <?php include('content.php') ?>
+      
+      <?php include('functions.php') ?>
+      <?php include('page.php') ?>
+      <script type="text/javascript" src='scripts.js'></script>
+   </body>
+</html>
